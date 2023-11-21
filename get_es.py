dd478eea30ccf633c73bc6f721dbc68c1a3adfa4
Sreenath D <sreenathd2002@gmail.com>
2023-11-21 08:44:55 +0530

    Create get_es.py

---
+++ get_es.py
@@ -0,0 +1,16 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+json_data = { 'query': { 'match_all': {}, }, }
+
+response = requests.get(
+    'https://aos-c7ec1282e2ca-gunzs7qzpw4cqz4xbc5bzfwgeu.us-east-1.es.amazonaws.com/customer-rejection-event-caisetl/_search?pretty',
+    headers=headers,
+    json=json_data,
+)
+
+for item in zip(data1['hits']['hits']):
+    print(item['_source']['initialCausedBySubmissionId'])
